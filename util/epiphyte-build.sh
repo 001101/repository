3c49de56d32656103a79eb320352aaee369e4087
Sean Enck <enckse@gmail.com>
2017-06-22 09:37:51 -0400

    generate an html page

--- util/epiphyte-build.sh
+++ util/epiphyte-build.sh
@@ -66,4 +66,88 @@ if [ $? -ne 0 ]; then
 fi
 WORKING=meta.md
 tar -xf $tar_xz .PKGINFO --to-stdout | grep -v "^#" > $WORKING
+_get_value()
+{
+    cat $WORKING | grep "^$1 =" | cut -d "=" -f 2 | sed "s/[[:space:]]*//g"
+}
+
+ADJUSTED="cleaned.md"
+source PKGBUILD
+
+echo "# $pkgname ("$(_get_value "pkgver")")" > $ADJUSTED
+
+echo "
+---
+
+$pkgdesc
+
+<a href='$url'>$url</a>
+
+" >> $ADJUSTED
+
+echo "| details | |
+| --- | --- |" >> $ADJUSTED
+
+echo "| built | "$(date -d @$(_get_value "builddate") +%Y-%m-%d)" |" >> $ADJUSTED
+cat $WORKING | grep -v -E "^(pkgname|pkgver|pkgdesc|url|makedepend|depend|builddate)" | sed "s/^/| /g;s/$/ |/g" | sed "s/=/|/g" >> $ADJUSTED
+
+has_depends=0
+for d in $(_get_value "depend"); do
+    if [ $has_depends -eq 0 ]; then
+    echo "
+## dependencies
+" >> $ADJUSTED
+    fi
+    has_depends=1
+    echo "* $d" >> $ADJUSTED
+done
+
+HTML_START="
+<html>
+<head>
+    <title>$pkgname</title>
+<style>
+
+html {
+    margin: auto;
+}
+
+body {
+    margin-top: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 85%; 
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
+</head>
+<body>
+"
+
+HTML_END="
+</body>
+</html>
+"
+
+mv $ADJUSTED $WORKING
+WORK_HTML=$WORKING.html
+pandoc $WORKING --out $WORK_HTML
+if [ $? -ne 0 ]; then
+    echo "unable to build html page"
+    exit 1
+fi
+
+OUT_HTML=$pkgname.html
+echo $HTML_START > $OUT_HTML
+cat $WORK_HTML >> $OUT_HTML
+echo $HTML_END >> $OUT_HTML
+
 cd $cwd
