93efa72f0770d654d01e3dc47e6af024f3242548
Sean Enck <enckse@gmail.com>
2017-09-06 10:48:15 -0400

    systemd cat the results

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CAT="autobuild"
 _build() {
     cwd=$PWD
     echo "workingdir: $tmp"
@@ -6,14 +7,16 @@ _build() {
     git clone https://github.com/epiphyte/pkgbuilds $tmp
     cd $tmp
     has=$(git log --after=$(date -d "30 minutes ago" +%Y-%m-%dT%H:%M:%S))
+    echo "$has" | systemd-cat -t "$CAT"
     if [ -z "$has" ]; then
         echo "nothing to be done"
     else
         for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers" | sort | uniq); do
-            echo "building: $f"
+            echo "building: $f" | systemd-cat -t "$CAT"
             cd $(echo $f | sed "s/PKGBUILD//g")
             epiphyte-package
             if [ $? -ne 0 ]; then
+                echo "failed" | systemd-cat -t "$CAT"
                 echo "failed build: $f" | smirc
             fi
             cd $tmp
