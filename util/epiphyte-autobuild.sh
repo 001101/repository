a2d421568e5039022539856e177724e3e44f8203
Sean Enck <enckse@gmail.com>
2017-09-06 12:13:52 -0400

    only build changes

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -12,12 +12,24 @@ _build() {
     _log "workingdir: $tmp"
     git clone https://github.com/epiphyte/pkgbuilds $tmp
     cd $tmp
-    has=$(git log --after=$(date -d "30 minutes ago" +%Y-%m-%dT%H:%M:%S))
+    git_cmd='git log --after=$(date -d "30 minutes ago" +%Y-%m-%dT%H:%M:%S'
+    has=$($git_cmd)
     _log "$has"
     if [ -z "$has" ]; then
         _log "noop"
     else
+        files=$($git_cmd" --pretty=format: --name-only" | sed "/^$/d" | grep "PKGBUILD" | sort | uniq)
         for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers" | sort | uniq); do
+            build=1
+            for m in $(echo "$files"); do
+                use_name="./$f"
+                if [ "$use_name" == "$m" ]; then
+                    build=0
+                fi
+            done
+            if [ $build -eq 1 ]; then
+                continue
+            fi
             _log "building: $f" 
             cd $(echo $f | sed "s/PKGBUILD//g")
             epiphyte-package
