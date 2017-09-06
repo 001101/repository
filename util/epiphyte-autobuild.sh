0d7f5e245ac6374d84b93e0a6125775482492218
Sean Enck <enckse@gmail.com>
2017-09-06 09:53:31 -0400

    report to smirc on error

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -3,5 +3,8 @@ cwd=$PWD
 for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers"); do
     cd $(echo $f | sed "s/PKGBUILD//g")
     epiphyte-package
+    if [ $? -ne 0 ]; then
+        echo "failed build: $f" | smirc
+    fi
     cd $cwd
 done
