28bc6620a5e3f5e08698cab78d12b16120a68032
Sean Enck <enckse@gmail.com>
2017-09-06 10:09:06 -0400

    auto-build

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cwd=$PWD
+echo "workingdir: $tmp"
 tmp=$(mktemp -d)
 git clone https://github.com/epiphyte/pkgbuilds $tmp
 cd $tmp
 for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers"); do
+    echo "building: $f"
     cd $(echo $f | sed "s/PKGBUILD//g")
     epiphyte-package
     if [ $? -ne 0 ]; then
