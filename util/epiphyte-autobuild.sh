659a6b2e59d1c35168f44520c710ffc310144c83
Sean Enck <enckse@gmail.com>
2017-09-06 12:26:13 -0400

    git control, filtering, and checking all in place and working

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -6,24 +6,27 @@ _log() {
     echo "$@" | tee -a $LAST_RUN | systemd-cat -t "$CAT"
 }
 
+_git() {
+    git log --after=$(date -d "30 minutes ago" +%Y-%m-%dT%H:%M:%S) $@
+}
+
 _build() {
     cwd=$PWD
     tmp=$(mktemp -d)
     _log "workingdir: $tmp"
     git clone https://github.com/epiphyte/pkgbuilds $tmp
     cd $tmp
-    git_cmd='git log --after=$(date -d "30 minutes ago" +%Y-%m-%dT%H:%M:%S'
-    has=$($git_cmd)
+    has=$(_git)
     _log "$has"
     if [ -z "$has" ]; then
         _log "noop"
     else
-        files=$($git_cmd" --pretty=format: --name-only" | sed "/^$/d" | grep "PKGBUILD" | sort | uniq)
+        files=$(_git "--pretty=format: --name-only" | sed "/^$/d" | grep "PKGBUILD" | sort | uniq)
         for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers" | sort | uniq); do
             build=1
             for m in $(echo "$files"); do
-                use_name="./$f"
-                if [ "$use_name" == "$m" ]; then
+                use_name="./$m"
+                if [ "$use_name" == "$f" ]; then
                     build=0
                 fi
             done
