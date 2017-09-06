1eb54bafd2c6418b51d6c89e58330e64c3f4881e
Sean Enck <enckse@gmail.com>
2017-09-06 10:35:20 -0400

    updates

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-cwd=$PWD
-echo "workingdir: $tmp"
-tmp=$(mktemp -d)
-git clone https://github.com/epiphyte/pkgbuilds $tmp
-cd $tmp
-has=$(git log --after=$(date -d "15 minutes ago" +%Y-%m-%dT%H:%M:%S))
-if [ -z "$has" ]; then
-    echo "nothing to be done"
-else
-    for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers"); do
-        echo "building: $f"
-        cd $(echo $f | sed "s/PKGBUILD//g")
-        epiphyte-package
-        if [ $? -ne 0 ]; then
-            echo "failed build: $f" | smirc
-        fi
-        cd $tmp
-    done
-fi
-cd $cwd
-rm -rf $tmp
+_build() {
+    cwd=$PWD
+    echo "workingdir: $tmp"
+    tmp=$(mktemp -d)
+    git clone https://github.com/epiphyte/pkgbuilds $tmp
+    cd $tmp
+    has=$(git log --after=$(date -d "15 minutes ago" +%Y-%m-%dT%H:%M:%S))
+    if [ -z "$has" ]; then
+        echo "nothing to be done"
+    else
+        for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers" | sort | uniq); do
+            echo "building: $f"
+            cd $(echo $f | sed "s/PKGBUILD//g")
+            epiphyte-package
+            if [ $? -ne 0 ]; then
+                echo "failed build: $f" | smirc
+            fi
+            cd $tmp
+        done
+    fi
+    cd $cwd
+    rm -rf $tmp
+}
+
+_build > /dev/null 2>&1
