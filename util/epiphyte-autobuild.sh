1932f0fefc2aadc6106034d427e424804479f93b
Sean Enck <enckse@gmail.com>
2017-09-06 10:13:38 -0400

    only build sometimes

--- util/epiphyte-autobuild.sh
+++ util/epiphyte-autobuild.sh
@@ -4,12 +4,19 @@ echo "workingdir: $tmp"
 tmp=$(mktemp -d)
 git clone https://github.com/epiphyte/pkgbuilds $tmp
 cd $tmp
-for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers"); do
-    echo "building: $f"
-    cd $(echo $f | sed "s/PKGBUILD//g")
-    epiphyte-package
-    if [ $? -ne 0 ]; then
-        echo "failed build: $f" | smirc
-    fi
-    cd $cwd
-done
+has=$(git log --after=$(date -d "10 minutes ago" +%Y-%m-%dT%H:%M:%S))
+if [ -z "$has" ]; then
+    echo "nothing to be done"
+else
+    for f in $(find . -type f | grep "PKGBUILD" | grep -v "bin/" | grep -v "containers"); do
+        echo "building: $f"
+        cd $(echo $f | sed "s/PKGBUILD//g")
+        epiphyte-package
+        if [ $? -ne 0 ]; then
+            echo "failed build: $f" | smirc
+        fi
+        cd $tmp
+    done
+fi
+cd $cwd
+rm -rf $tmp
