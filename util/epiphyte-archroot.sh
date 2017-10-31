c11e0f0eeec8d1a26cbba515901b483d182abf92
Sean Enck <enckse@gmail.com>
2017-10-31 12:39:34 -0400

    resolves #9

--- util/epiphyte-archroot.sh
+++ util/epiphyte-archroot.sh
@@ -3,4 +3,17 @@ if [ -z "$CHROOT" ]; then
     echo "no CHROOT environment variable set"
     exit 1
 fi
-arch-nspawn -C /etc/pacman.conf $CHROOT/root pacman -Syyu
+UPDATE="update"
+BUILD="build"
+
+case $1 in
+    $UPDATE)
+        arch-nspawn -C /etc/pacman.conf $CHROOT/root pacman -Syyu
+        ;;
+    $BUILD)
+        mkarchroot -C /etc/pacman.conf $CHROOT/root base-devel
+        ;;
+    *)
+        echo "must run with $BUILD or $UPDATE"
+        ;;
+esac
