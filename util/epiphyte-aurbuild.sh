e3405ee75da40713e5897ac35ac542af2ab1cdcf
Sean Enck <enckse@gmail.com>
2017-11-15 13:19:40 -0500

    fixing operations

--- util/epiphyte-aurbuild.sh
+++ util/epiphyte-aurbuild.sh
@@ -7,15 +7,18 @@ _build() {
     cwd=$PWD
     pkgs=""
     for b in $(cat $PKGBUILD); do
+        _file=$b.tar.gz
         rm -f $_file
         _tmp=$(mktemp -d)
         echo "building $b in $_tmp"
         cd $_tmp
-        _file=$b.tar.gz
         curl https://aur.archlinux.org/cgit/aur.git/snapshot/$b.tar.gz > $_file
         tar xf $_file
         cd $b
-        makepkg -sr --noconfirm
+        epiphyte-package x86_64
+        if [ $? -ne 0 ]; then
+            echo "failed aur build: $b" | smirc
+        fi
         for f in $(ls *.pkg.tar.xz); do
             pkgs=$pkgs" $f"
             rsync -avc $f $CACHE
@@ -24,6 +27,9 @@ _build() {
     cd $CACHE
     if [ ! -z "$pkgs" ]; then
         repo-add -n auriphyte.db.tar.gz $pkgs
+        if [ $? -ne 0 ]; then
+            echo "unable to update: $b" | smirc
+        fi
     fi
     cd $cwd
 }
